1f531d3ec2e7c112281129e2505278749d2fc0d6
peridot0810 <163098646+peridot0810@users.noreply.github.com>
2024-10-12 02:05:14 +0900

    제품 등록기능 + 제품 상세정보 창 추가

--- DB_handler.py
+++ DB_handler.py
@@ -7,17 +7,18 @@ class DBModule:
     with open("./auth/firebaseAuth.json") as f:
       config = json.load(f)
     
-    firebase = pyrebase.initialize_app(config)   # 데이터베이스 앱 초기화
-    self.db = firebase.database()                # 데이터베이스 연결
+    firebase = pyrebase.initialize_app(config)     # 데이터베이스 앱 초기화
+    self.db = firebase.database()                  # 데이터베이스 연결
 
-  def signin_verification(self, uid):
+  # ======= 회원가입 =======
+  def signin_verification(self, uid):              # 회원가입 검증
     users = self.db.child("users").get().val()
     try:
       for i in users:
-        if uid == i:
+        if uid == i:                               # 중복 ID가 있다면 False 반환
           return False
       return True
-    except:
+    except:                                        # except : 회원이 한명도 없는 경우 (users가 None)
       return True
 
   def signin(self, uid, pwd, name):
@@ -30,15 +31,58 @@ class DBModule:
       return True
     else:
       return False
-    
+  # =========================
+
+
+
+  # ===== 로그인 ======
   def login(self, uid, pwd):
     users = self.db.child("users").get().val()
     try:
-      userinfo = users[uid]
+      userinfo = users[uid]                         # userinfo : 딕셔너리
       if pwd == userinfo["pwd"]:
         return True
       else:
         return False
+    except:                                         # except : 회원이 한명도 없는 경우 (users가 None)
+      return False
+  # =================
+
+
+  
+  # ===== 제품 등록 =====
+  def registration_verification(self, pid):          # 제품 등록 검증 (pid : product id가 겹치는 경우는 없는지 확인)
+    products = self.db.child("products").get().val()
+    try:
+      for i in products:
+        if pid == i:
+          return False
+      return True
+    except:                                          # except : 제품이 하나도 없는 경우 (products가 None)
+      return True
+
+  def product_registration(self, pid, price, product_name):
+    information = {
+      "price" : price,
+      "product_name" : product_name
+    }
+    if self.registration_verification(pid):
+      self.db.child("products").child(pid).set(information)
+      return True
+    else:
+      return False
+  # ==================
+
+
+  # ===== 제품 정보 가져오기 =====
+  def get_products(self):             # 모든 제품 목록 가져오기
+    try:
+      products = self.db.child("products").get().val()
+      return products
     except:
       return False
-      
+
+  def product_detail(self, pid):      # 개별 제품의 세부정보 가져오기
+    product_info = self.db.child("products").get().val()[pid]
+    return product_info
+  # ===========================
--- app.py
+++ app.py
@@ -1,22 +1,34 @@
 from flask import Flask, redirect, render_template, url_for, request, flash, session
 from DB_handler import DBModule
 
+# ==== 초기 설정 ====
 app = Flask(__name__)
-app.secret_key = "as;dlkjgao;psjr"   # 아무거나 친거. 유출되면 안되는 키
-DB = DBModule()  # 데이터베이스 객체
+app.secret_key = "as;dlkjgao;psjr"   # 아무거나 친 코드. 유출되면 안됨
+DB = DBModule()                      # DB모듈 인스턴스 생성
+# =================
 
-@app.route("/")  # 홈페이지
+
+
+# ===== 메인 페이지 =====
+@app.route("/")
 def index():
-  if "uid" in session:
+  if "uid" in session:               # 로그인이 되어있다면  -> user = 유저 아이디
     user = session['uid']
-  else:
+  else:                              # 로그인되어있지 않다면 -> user = "Login_needed"
     user = "Login_needed"
-  return render_template("index.html", user = user)
+
+  products = DB.get_products()
+  if not products:                   # product가 없음 -> 제품 가져오기 실패
+    return render_template("index.html", user = user, products = "No_products")
+  return render_template("index.html", user = user, products = products)  # index.html에 user, products 넘기기
+# =====================
 
 
-@app.route("/signin") # 회원가입창
+
+# ===== 회원가입 =====
+@app.route("/signin") 
 def signin():
-  if "uid" in session:
+  if "uid" in session:               # 로그인 상태라면 redirect -> 메인페이지
     return redirect(url_for("index"))
   return render_template("signin.html")
 
@@ -26,16 +38,19 @@ def signin_done():
   pwd = request.args.get("pwd")
   name = request.args.get("name")
   
-  if DB.signin(uid, pwd, name):  # 회원가입 성공
+  if DB.signin(uid, pwd, name):      # 회원가입 성공
     return redirect(url_for("index"))
   else:
-    flash("이미 존재하는 아이디 입니다")
+    flash("이미 존재하는 아이디 입니다")    # 회원가입 실패
     return redirect(url_for("signin"))
+# ======================
 
 
+
+# ===== 로그인/로그아웃 =====
 @app.route("/login")  # 로그인창
 def login():
-  if "uid" in session:
+  if "uid" in session:                # 로그인 상태라면 redirect -> 메인페이지
     return redirect(url_for("index"))
   return render_template("login.html")
 
@@ -43,23 +58,53 @@ def login():
 def login_done():
   uid = request.args.get("id")
   pwd = request.args.get("pwd")
-  if DB.login(uid, pwd):
+  if DB.login(uid, pwd):              # 로그인 성공 -> session["uid"] = 유저 아이디
     session["uid"] = uid
     return redirect(url_for("index"))
-  else:
+  else:                               # 로그인 실패
     flash("아이디가 없거나 틀린 비밀번호 입니다")
     return redirect(url_for("login"))
-  
+
 @app.route("/logout")
 def logout():
-  if "uid" in session:
+  if "uid" in session:                # 로그인 상태라면 session에서 "uid"라는 key를 pop
     session.pop("uid")
     return redirect(url_for("index"))
-  else:
+  else:                               # 로그인 상태가 아니라면 redirect -> 로그인 창
     return redirect(url_for("login"))
+# =================================
+  
+
+
+# ====== 제품 등록 ======
+@app.route("/product_registration")  
+def product_registration():
+  if "uid" not in session or session["uid"] != "root":   # 로그인 되어있고, 유저 아이디가 "root"일때만 제품 등록 가능 
+    return redirect(url_for("index"))
+  return render_template("product_registration.html")
+
+@app.route("/registration_done", methods=["get"])  
+def registration_done():
+  pid = request.args.get("id")
+  price = request.args.get("price")
+  product_name = request.args.get("name")
+  if DB.product_registration(pid, price, product_name):  # 제품 등록 성공
+    return redirect(url_for("index"))
+  else:                                                  # 제품 등록 실패
+    flash("제품 ID가 중복됩니다")
+    return redirect(url_for("product_registration"))
+# =======================
 
 
 
+# ===== 개별 제품 상세정보 =====
+@app.route("/product_detail/<string:pid>")  # /product_detail/제품 ID
+def product_detail(pid):
+  product_info = DB.product_detail(pid)
+  return render_template("product_detail.html", product_info=product_info, pid=pid)
+# ==========================
 
+
+# ===== 서버 실행 =====
 if __name__ == "__main__":
   app.run(host = "0.0.0.0", debug=True)
