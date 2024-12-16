5bc25d6369a5d3003cbf38ce0df846fda6108089
sebinyday <jjnf0219@gmail.com>
2024-12-16 13:27:24 +0900

    메인페이지 완성 전

--- reactver/src/App.jsx
+++ reactver/src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import {
   Routes,
   Route,
@@ -17,41 +17,46 @@ function App() {
   const location = useLocation();
   const pathname = location.pathname;
 
+  // 페이지 이동 시 스크롤 위치 초기화
   useEffect(() => {
     if (action !== "POP") {
       window.scrollTo(0, 0);
     }
   }, [action, pathname]);
 
+  // 동적으로 제목 및 메타 설명 변경
   useEffect(() => {
     let title = "";
     let metaDescription = "";
 
     switch (pathname) {
       case "/":
-        title = "";
-        metaDescription = "";
+        title = "홈페이지";
+        metaDescription = "이곳은 홈페이지입니다.";
         break;
       case "/log-in":
-        title = "";
-        metaDescription = "";
+        title = "로그인";
+        metaDescription = "로그인 페이지입니다.";
         break;
       case "/category-page":
-        title = "";
-        metaDescription = "";
+        title = "카테고리";
+        metaDescription = "카테고리 페이지입니다.";
         break;
       case "/product-detail-page":
-        title = "";
-        metaDescription = "";
+        title = "상품 상세";
+        metaDescription = "상품 상세 페이지입니다.";
         break;
       case "/sign-up":
-        title = "";
-        metaDescription = "";
+        title = "회원가입";
+        metaDescription = "회원가입 페이지입니다.";
         break;
       case "/cart":
-        title = "";
-        metaDescription = "";
+        title = "장바구니";
+        metaDescription = "장바구니 페이지입니다.";
         break;
+      default:
+        title = "React와 Flask 연동";
+        metaDescription = "React와 Flask를 연동한 애플리케이션입니다.";
     }
 
     if (title) {
@@ -79,4 +84,117 @@ function App() {
     </Routes>
   );
 }
+
 export default App;
+
+
+// import React, { useEffect, useState } from "react";
+// import axios from "axios";
+
+// function App() {
+//     const [data, setData] = useState(null);
+
+//     useEffect(() => {
+//         // Flask API 호출
+//         axios.get("http://localhost:5000/api/data")
+//             .then((response) => {
+//                 setData(response.data.message); // "Hello from Flask!" 가져오기
+//             })
+//             .catch((error) => {
+//                 console.error("Error fetching data:", error);
+//             });
+//     }, []);
+
+//     return (
+//         <div>
+//             <h1>React와 Flask 연동</h1>
+//             {data ? <p>Flask 응답: {data}</p> : <p>Loading...</p>}
+//         </div>
+//     );
+// }
+
+// export default App;
+
+
+// // import { useEffect } from "react";
+// import {
+//   Routes,
+//   Route,
+//   useNavigationType,
+//   useLocation,
+// } from "react-router-dom";
+// import Homepage from "./pages/Homepage";
+// import LogIn from "./pages/LogIn";
+// import CategoryPage from "./pages/CategoryPage";
+// import ProductDetailPage from "./pages/ProductDetailPage";
+// import SignUp from "./pages/SignUp";
+// import Cart from "./pages/Cart";
+
+// function App() {
+//   const action = useNavigationType();
+//   const location = useLocation();
+//   const pathname = location.pathname;
+
+//   useEffect(() => {
+//     if (action !== "POP") {
+//       window.scrollTo(0, 0);
+//     }
+//   }, [action, pathname]);
+
+//   useEffect(() => {
+//     let title = "";
+//     let metaDescription = "";
+
+//     switch (pathname) {
+//       case "/":
+//         title = "";
+//         metaDescription = "";
+//         break;
+//       case "/log-in":
+//         title = "";
+//         metaDescription = "";
+//         break;
+//       case "/category-page":
+//         title = "";
+//         metaDescription = "";
+//         break;
+//       case "/product-detail-page":
+//         title = "";
+//         metaDescription = "";
+//         break;
+//       case "/sign-up":
+//         title = "";
+//         metaDescription = "";
+//         break;
+//       case "/cart":
+//         title = "";
+//         metaDescription = "";
+//         break;
+//     }
+
+//     if (title) {
+//       document.title = title;
+//     }
+
+//     if (metaDescription) {
+//       const metaDescriptionTag = document.querySelector(
+//         'head > meta[name="description"]'
+//       );
+//       if (metaDescriptionTag) {
+//         metaDescriptionTag.content = metaDescription;
+//       }
+//     }
+//   }, [pathname]);
+
+//   return (
+//     <Routes>
+//       <Route path="/" element={<Homepage />} />
+//       <Route path="/log-in" element={<LogIn />} />
+//       <Route path="/category-page" element={<CategoryPage />} />
+//       <Route path="/product-detail-page" element={<ProductDetailPage />} />
+//       <Route path="/sign-up" element={<SignUp />} />
+//       <Route path="/cart" element={<Cart />} />
+//     </Routes>
+//   );
+// }
+// export default App;
---
+++ sebin/Server.py
@@ -0,0 +1,181 @@
+from DB import DBModule
+from flask import Flask, redirect, render_template, url_for, request, flash, session
+from Users import Consumer, Designer, Root
+
+
+class Server:
+  def __init__(self):
+    self.db = DBModule()
+    self.User = None
+    
+
+  # ======== 회원가입 ==========
+  def sign_in(self, user_info : dict, img):
+    if self.db.sign_in(user_info, img):
+      return True
+    else:
+      return False
+  # ============================
+  
+
+
+  # ========= 로그인/로그아웃 ============
+  def log_in(self, login_info : dict):
+    User_info = self.db.log_in(login_info)  # User 정보를 dict 형태로 반환
+    if User_info:    # 로그인 성공
+      self.make_User(User_info)
+      # 세션 열기
+      session["id"] = User_info["id"]
+      session["type"] = User_info["type"]
+      return True
+    else:                             # 로그인 실패
+      return False
+    
+  def log_out(self):
+    self.User = None
+    session.pop("id")
+    session.pop("type")
+    return True
+  # ==================================
+
+
+
+
+  # ============= 로그인 여부 체크 =============
+  def check_login(self):
+    if "id" in session:                # 로그인이 되어있다면  -> user = 유저 아이디, user_type = 사용자 타입
+      return self.User
+    else:                              # 로그인되어있지 않다면 -> None
+      raise Exception("로그인 상태가 아닙니다")
+  # ========================================
+    
+
+
+  # ========= 제품 등록 ===============
+  def product_registration(self, product_info, product_img, type, did=None, category=None):
+    if self.db.product_registration(product_info, product_img, type, did, category):
+      if type == "Designer":
+        self.User.products[product_info["pid"]] = product_info
+        print(self.User.products)
+      return True
+    else:
+      return False
+  # ========================================
+
+
+
+  # =============== 제품 get ================
+  def get_products(self, category=None):
+    try:
+      user_type = self.User.type
+    except:
+      user_type = None
+
+    if user_type == "Designer":
+      products = self.db.get_products(did = self.User.id)
+    elif category != None:  # 특정 카테고리의 제품만 가져오기
+      products = self.db.get_products(category=category)
+    else:                   # 모든 제품 가져오기
+      products = self.db.get_products()
+    return products
+  
+  def get_product_detail(self, pid, did=None, category=None):
+    product_detail = self.db.get_product_detail(pid, did = did, category=category)
+    return product_detail
+  
+  # Consumer를 위한 함수 -> Designer는 사용 안하는 것으로 생각
+  def get_products_by(self, by, category=None, num=None, sort=None):
+    products = self.db.get_products_by(by, category=category, num=num, sort=sort)
+    return products
+  # ========================================
+
+
+
+
+  # =============== 사용자 get ===============
+  def get_users(self, type):
+    users = self.db.get_users(type)
+    if users:
+      return users
+    else:
+      return None
+
+  def get_user_detail(self, id, type):
+    user = self.db.get_user_detail(id, type)
+    if user:
+      return user
+    else:
+      return None
+  # =========================================
+
+
+
+
+  # ========== 유저/제품 삭제 =========
+  def user_delete(self, type, id):
+    return self.db.user_delete(type, id)
+    
+  def product_delete(self, type, pid, did=None, category=None):
+    print(category)
+    if self.db.product_delete(type, pid, did=did, category=category):
+      if type == "Designer":
+        del self.User.products[pid]
+        print(self.User.products)
+      return True
+    else:
+      return False
+
+  # ================================
+
+
+
+
+  # ========== 유저 정보 수정 ==========
+  def edit_info(self, id, type, update_info):
+    new_user_info = self.db.edit_info(id, type, update_info)
+    if new_user_info:
+      print(new_user_info)
+      self.log_out()
+      self.log_in(new_user_info)
+      # self.User 업데이트
+      self.User.id = new_user_info["id"]
+      self.User.name = new_user_info["name"]
+      self.User.pwd = new_user_info["pwd"]
+      self.User.phone = new_user_info["phone"]
+      self.User.email = new_user_info["email"]
+      if type == "Consumer":
+        self.User.img_path = new_user_info["img_path"]
+      elif type == "Deisnger":
+        self.User.dir_path = new_user_info["img_path"]
+      return True
+    else:
+      return False
+    
+  def upload_img(self, img, cid):
+    img_path = self.db.upload_img(img, cid)
+    if img_path:
+      self.User.img_path = img_path
+      return True
+    else:
+      return False
+  # ==================================
+
+
+
+
+  # =========== 기타 함수 ==============
+  def make_User(self, User_info):
+    type = User_info["type"]
+    if type == "Consumer":
+      self.User = Consumer(User_info["id"], User_info["pwd"], User_info["name"], User_info["phone"], User_info["email"])
+      self.User.img_path = self.get_user_detail(User_info["id"], "Consumer")["img_path"]
+      print(self.User.img_path)
+    elif type == "Designer":
+      self.User = Designer(User_info["id"], User_info["pwd"], User_info["name"], User_info["phone"], User_info["email"])
+      products = self.get_products()
+      if products:
+        self.User.products = products
+      print(self.User.products)
+    elif type == "Root":
+      self.User = Root(User_info["id"], User_info["pwd"], User_info["name"])
+  # ===================================
