4d01622a2a3c4631518d2865bb1c8c512aeb735b
sebinyday <jjnf0219@gmail.com>
2024-12-16 15:35:41 +0900

    메인페이지 배너 완료

--- sebin/static/js/header.js
+++
@@ -1,8 +0,0 @@
-function closeHeader() {
-    var header = document.getElementById("topHeader"); // ID가 올바른지 확인
-    if (header) {
-        header.style.display = "none"; // 헤더를 숨김
-    } else {
-        console.error("Element with ID 'topHeader' not found");
-    }
-}
