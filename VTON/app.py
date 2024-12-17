0aca3c9886896b7ae41723349b9e05d26cf52f5d
sebinyday <jjnf0219@gmail.com>
2024-12-17 20:48:30 +0900

    maybe final

--- VTON/app.py
+++ VTON/app.py
@@ -31,7 +31,7 @@ def get_gradio_client():
     """Gradio Client를 필요할 때만 초기화"""
     global gradio_client
     if gradio_client is None:
-        gradio_client = GradioClient("yisol/Virtual-Try-On")
+        gradio_client = GradioClient("yisol/IDM-VTON")
     return gradio_client
 
 def call_gradio_api(person_image_path, garment_image_path):
